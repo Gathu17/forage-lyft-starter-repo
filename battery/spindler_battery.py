be69a3affe63a0b39b15a1d4827be6a5fa87a724
jayGathu <jjgathu17@gmail.com>
2023-01-30 19:39:51 +0300

    refractoring

---
+++ battery/spindler_battery.py
@@ -0,0 +1,16 @@
+from abc import ABC
+from car import Car
+
+
+
+class SpindlerBattery(Car,ABC):
+    def __init__(self,last_service_date,current_date):
+      super().__init__(last_service_date)
+      self.current_date = current_date
+      self.last_service_date = last_service_date
+    def needs_service(self):
+      date_to_be_serviced = self.last_service_date.replace(year = self.last_service_date.year + 2)
+      if date_to_be_serviced < self.current_date:
+        return False
+      else:
+        return True
--- engine/battery/spindler_battery.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC
-from car import Car
-import datetime
-
-class SpindlerBattery(Car,ABC):
-    def __init__(self,last_service_date,current_date):
-      super().__init__(last_service_date)
-      self.current_date = datetime.date.today()
